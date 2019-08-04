b4cb1f2ae78db9fac11ba8d884b470379e10b864
Ali Momin <alimomin103@gmail.com>
2019-08-04 15:44:01 -0500

    m

--- src/main/java/com/jivatech/replacehost/ReplacehostApplication.java
+++ src/main/java/com/jivatech/replacehost/ReplacehostApplication.java
@@ -1,13 +1,34 @@
 package com.jivatech.replacehost;
 
+
+import com.jivatech.replacehost.repository.entity.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
 @SpringBootApplication
-public class ReplacehostApplication {
+public class ReplacehostApplication extends Customer
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(ReplacehostApplication.class, args);
+
+        String newName;
+
+        Scanner input = new Scanner(System.in);
+
+        Customer customer = new Customer();
+
+        System.out.print("Enter your full name");
+        customer.setName(input.next());
+        
+
+
+
+
+
     }
+
 
 }
