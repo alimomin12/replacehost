ae4293142ffbfe05988ffe20a4acdc0690ce86dd
Ali Momin <alimomin103@gmail.com>
2020-03-02 16:05:30 -0600

    update

---
+++ src/main/java/com/jivatech/replacehost/repository/entity/Order.java
@@ -0,0 +1,9 @@
+package com.jivatech.replacehost.repository.entity;
+
+
+import lombok.Data;
+
+@Data
+public class Order {
+    
+}
---
+++ src/main/java/com/jivatech/replacehost/repository/impl/TableRepositoryImpl.java
@@ -0,0 +1,57 @@
+package com.jivatech.replacehost.repository.impl;
+
+import com.jivatech.replacehost.repository.TableRepository;
+import com.jivatech.replacehost.repository.entity.Table;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+
+@Repository
+public class TableRepositoryImpl implements TableRepository {
+
+    ArrayList<Table> tables;
+
+    @PostConstruct
+    void setup() {
+        tables = new ArrayList<>();
+        Table t1 = new Table();
+        t1.setTableLocation("bathroom");
+        t1.setTableNumber(1);
+        t1.setTableSize(5);
+        t1.setIsAvailable(true);
+        tables.add(t1);
+
+        Table t2 = new Table();
+        t2.setTableLocation("kitchen");
+        t2.setTableNumber(2);
+        t2.setTableSize(5);
+        t2.setIsAvailable(true);
+        tables.add(t2);
+    }
+
+
+    @Override
+    public Table findTable(Boolean isAvailable, Integer partySize) {
+
+        // return tables.stream().filter(table -> (table.getIsAvailable() == isAvailable) && (table.getTableSize() >= partySize)).findFirst().orElse(null);
+
+        Table returnVal = null;
+        for (Table table : tables) {
+            if ((java.util.Objects.equals(table.getIsAvailable(), isAvailable)) &&
+                    (table.getTableSize().intValue() >= partySize.intValue())) {
+                returnVal = table;
+            }
+        }
+        return returnVal;
+    }
+
+    @Override
+    public void changeTableAvailability(Integer tableNumber, Boolean newAvailability) {
+        for (Table table : tables) {
+            if(java.util.Objects.equals(table.getTableNumber(), tableNumber)) {
+                table.setIsAvailable(newAvailability);
+            }
+        }
+    }
+}
